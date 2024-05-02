a8e7065e7f521099ffc07d8907f483e2a73365d8
David Reis <eudavidreis.dev@gmail.com>
2024-05-02 17:40:12 -0300

    Feat - Inicio da implementação da interface da tela inicial.

--- gerador-de-senha/App.js
+++ gerador-de-senha/App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Image, Button} from 'react-native';
+import Slider from '@react-native-community/slider'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require('./assets/logo.png')}
+          style={styles.logo}>
+
+      </Image>
+      <Text style={styles.tittle}>20 caracteres</Text>
+
+      <View style={styles.area}>
+        <Slider style={styles.slider}></Slider>
+
+
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#ffffff',
     justifyContent: 'center',
+    alignItems: 'center'
   },
+  logo:{
+    marginBottom: 60
+  },
+  tittle:{
+
+  },
+  area:{
+    marginTop:14,
+    marginBottom:14,
+    width: '80%',
+    alignItems:'center'
+
+  },
+  slider:{
+    width:'80%',
+    height: 50,
+    minimumValue: 6,
+    maximumValue:20,
+    backgroundColor:"#fff"
+  }
 });
