1cfde468c198d79dc5159ca21d8fc437bb0c362a
David Reis <eudavidreis.dev@gmail.com>
2024-05-21 16:55:49 -0300

    Feat - useState aplicado.

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
+import {useState} from 'react'
 import { StyleSheet, Text, View ,Image, Button, TouchableOpacity} from 'react-native';
 import Slider from '@react-native-community/slider'
 import styles from './src/stylesheets/base_stylesheet.js'
 
 export default function App() {
+  const [size, setSize] = useState(10)
+
   return (
     <View style={styles.container}>
       <Image source={require('./src/assets/logo.png')}
           style={styles.logo}>
 
       </Image>
-      <Text style={styles.tittle}>20 caracteres</Text>
+      <Text style={styles.tittle}>{size} caracteres</Text>
 
       <View style={styles.area}>
         <Slider style={styles.slider}
@@ -18,9 +21,9 @@ export default function App() {
           minimumValue={6}
           maximumTrackTintColor="#00ffff"
           minimumTrackTintColor="#000"
-          thumbTintColor='#5340ff'></Slider>
-
-
+          thumbTintColor='#5340ff'
+          value={size}
+          onValueChange={(value)=> setSize(value.toFixed(0))}></Slider>
       </View>
 
       <TouchableOpacity style={styles.button}>
